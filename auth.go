43d65400838a8ea408b45b7580dab7f3b8d29acd
Pranav Teegavarapu <pranavnt@outlook.com>
2021-02-26 20:56:24 -0800

    Added functions to hash and check password

--- auth.go
+++ auth.go
@@ -1,9 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func signUp(w http.ResponseWriter, r *http.Request) {}
+
+func hashPassword(password string) (hashedPassword string) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	hashedPassword = string(hash)
+	return
+}
+
+func checkPassword(hashedPassword string, guess string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(guess))
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pranavnt/Asteroid
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83
+)
