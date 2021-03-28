c2f829c463a2218510015a7bf53eb620556724dd
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-27 18:07:45 -0700

    json -> stringgit add .

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::data::FromDataSimple;
+use rocket::data;
+use rocket::Data;
+use rocket::Outcome::Failure;
+use rocket::Outcome::Success;
+use rocket::Request;
+use rocket::http::Status;
+use std::io::Read;
+
+struct Store {
+    contents: String,
+}
+
+impl FromDataSimple for Store {
+    type Error = String;
+
+    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+        let mut contents = String::new();
+
+        if let Err(e) = data.open().take(256).read_to_string(&mut contents) {
+            return Failure((Status::InternalServerError, format!("{:?}", e)));
+        }
+
+        Success(Store { contents })
+    }
+}
+
+#[post("/process", format = "application/json", data = "<input>")]
+fn process_store(input: Store) -> &'static str {
+    "200 Okey Dokey"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![process_store]).launch();
 }
