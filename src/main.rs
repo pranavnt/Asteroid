c31ebfb9a9adaa6bda784822322c46fd0a9cc4a6
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-27 19:29:05 -0700

    yay

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,12 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::data::FromDataSimple;
-use rocket::data::Outcome;
-use rocket::Data;
-use rocket::Outcome::Failure;
-use rocket::Outcome::Success;
-use rocket::Request;
-use rocket::http::Status;
-use std::io::Read;
-
-struct Store {
-    contents: String,
-}
-
-impl FromDataSimple for Store {
-    type Error = String;
-
-    fn from_data(req: &Request, data: Data) -> Outcome<Self, String> {
-        let mut contents = String::new();
-
-        if let Err(e) = data.open().take(256).read_to_string(&mut contents) {
-            return Failure((Status::InternalServerError, format!("{:?}", e)));
-        }
-
-        println!("{}",contents);
-
-        Success(Store { contents })
-    }
-}
+mod utils;
 
 #[post("/process", format = "application/json", data = "<input>")]
-fn process_store(input: Store) -> &'static str {
-    "200 Okey Dokey"
+fn process_store(input: utils::store::Store) -> &'static str {
+    println!("{}", input.contents);
+    "200"
 }
 
 fn main() {
