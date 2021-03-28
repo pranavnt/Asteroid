4e3ae5bbc47eed4b5bdccd7ef10803abce5176b6
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-27 18:11:11 -0700

    printing it out now

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 extern crate rocket;
 
 use rocket::data::FromDataSimple;
-use rocket::data;
+use rocket::data::Outcome;
 use rocket::Data;
 use rocket::Outcome::Failure;
 use rocket::Outcome::Success;
@@ -19,12 +19,14 @@ struct Store {
 impl FromDataSimple for Store {
     type Error = String;
 
-    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+    fn from_data(req: &Request, data: Data) -> Outcome<Self, String> {
         let mut contents = String::new();
 
         if let Err(e) = data.open().take(256).read_to_string(&mut contents) {
             return Failure((Status::InternalServerError, format!("{:?}", e)));
         }
+
+        println!("{}",contents);
 
         Success(Store { contents })
     }
