7ceec976d0879fc3d5dac22a59f8180d044a8540
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-29 21:13:23 -0700

    file utils

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -4,9 +4,12 @@ fn create_collection(collection_name: &str) {
     fs::create_dir_all("./.db/" + collection_name);
 }
 
-fn read_file_to_string(collection_name: &str, doc_id: &str) -> str {
-    return fs::read_to_string("/.db/" + collection_name + "/".doc_id + ".json")
+fn get_document(collection_name: &str, doc_id: &str) -> str {
+    return fs::read_to_string("/.db/" + collection_name + "/" + doc_id + ".json")
         .expect("Unable to read file");
 }
 
-fn dump_to_file(data: &str) -> bool {}
+fn create_document(data: &str, collection_name: &str, doc_id: &str) -> bool {
+    fs::write("/.db/" + collection_name + "/" + doc_id + ".json", data)
+        .expect("Unable to write file");
+}
