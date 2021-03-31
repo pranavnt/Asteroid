6cb27303b5b4e70745b80e8d6ce3aa39df2c372b
Pranav Teegavarapu <pranavnt@outlook.com>
2021-03-30 20:39:12 -0700

    f

--- src/utils/file.rs
+++ src/utils/file.rs
@@ -1,15 +1,18 @@
 use std::fs;
 
 fn create_collection(collection_name: &str) {
-    fs::create_dir_all("./.db/" + collection_name);
+    fs::create_dir_all(["./.db/", collection_name].to_owned());
 }
 
-fn get_document(collection_name: &str, doc_id: &str) -> str {
-    return fs::read_to_string("/.db/" + collection_name + "/" + doc_id + ".json")
+fn get_document(collection_name: &str, doc_id: &str) -> String {
+    return fs::read_to_string(["/.db/", collection_name, "/", doc_id, ".json"].concat())
         .expect("Unable to read file");
 }
 
-fn create_document(data: &str, collection_name: &str, doc_id: &str) -> bool {
-    fs::write("/.db/" + collection_name + "/" + doc_id + ".json", data)
-        .expect("Unable to write file");
+fn create_document(data: &str, collection_name: &str, doc_id: &str) {
+    fs::write(
+        concat!("/.db/", collection_name, "/", doc_id, ".json"),
+        data,
+    )
+    .expect("Unable to write file");
 }
