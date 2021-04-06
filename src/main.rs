53925cedd10ea424bc78032dabe21a2bb0953e34
Pranav Teegavarapu <pranavnt@outlook.com>
2021-04-05 22:37:52 -0700

    build works ig

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,16 @@ use serde_json::Value;
 mod utils;
 
 #[post("/document/create", format = "application/json", data = "<input>")]
-fn create_document(input: utils::store::Store) -> &'static str {
+fn document_create(input: utils::store::Store) -> &'static str {
     let req_body: Value = utils::json::str_to_json(&String::from(input.contents));
 
-    utils::file::create_document(&format!("{}",req_body), req_body["collection"], "hi");
-
-    "{\"sucesss\": true}"
-}
-
-#[post("/document/get", format = "application/json", data = "<input>")]
-fn create_document(input: utils::store::Store) -> &'static str {
-    let req_body: Value = utils::json::str_to_json(&String::from(input.contents));
-
-    // utils::file::create_document(&format!("{}",req_body), "test", "hi");
+    utils::file::create_document(&format!("{}",req_body), "test", "hi");
 
     println!("{}", req_body);
 
     "200"
 }
 
-
-
 fn main() {
-    utils::file::create_collection("test");
-    rocket::ignite().mount("/", routes![process_store]).launch();
+    rocket::ignite().mount("/", routes![document_create]).launch();
 }
