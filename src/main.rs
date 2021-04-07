3cadb968cc9b2ab11712a01b48fc4cd8921866ad
Pranav Teegavarapu <pranavnt@outlook.com>
2021-04-07 14:16:12 -0700

    Able to get doc (w/o params)

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod utils;
 fn document_create(input: utils::store::Store) -> &'static str {
     let req_body: Value = utils::json::str_to_json(&String::from(input.contents));
 
-    utils::file::create_document(&format!("{}",req_body), "test", "hi");
+    utils::file::create_document(&format!("{}", req_body), "test", "hi");
 
     println!("{}", req_body);
 
@@ -22,13 +22,11 @@ fn document_create(input: utils::store::Store) -> &'static str {
 fn document_get(input: utils::store::Store) -> String {
     let req_body: Value = utils::json::str_to_json(&String::from(input.contents));
 
-    let document = utils::file::get_document("todos", "hi");
-
-    println!("{}", req_body);
-
-    document
+    utils::file::get_document("test", "hi")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![document_create, document_get]).launch();
+    rocket::ignite()
+        .mount("/", routes![document_create, document_get])
+        .launch();
 }
